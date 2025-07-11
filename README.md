7c2e5fe62ceba0adb01079399ff0ed66e2233394
Sharath <sharath@MacBook-Air.local>
2025-07-11 23:13:28 +0530

    Add a number of user requested features to the app

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # Social-Blacklist
-A reddit mod tool to automatically remove posts by users who have social links belonging to certain domains. This would be helpful to communities where spamming is common by users with goal to attract visitors to their social-links for commercial gain(like platforms for selling content, or get PPV etc.). After installing the app, go to App Settings and configure the domains and removal message as per your requirement. To know if the app is removing the posts accordingly, you can check your mod-log for actions made by /u/social-blacklist.
+A reddit mod tool to blacklist a certain number of social domains/websites and automatate removal of posts based on the domains found in:
+ * `Social Links` section of their profile
+ * Post Link of the post
+ * Post Body of the post
+ 
+ This would be helpful to communities where spamming is common by users/bots with a goal to attract visitors to their sites/pages for commercial gain(like platforms for selling content, or get PPV etc.). After installing the app, go to App Installation Settings and configure the domains and removal message as per your requirement, and the enable/disable flags on what/where to look for these domains for removal. To know if the app is removing the posts accordingly, you can enable mod-mail notification on removal through the settings OR check your mod-log for actions made by /u/social-blacklist.
 
 ### Installing the app:
 Moderators can install the app to their subreddit by going to [https://developers.reddit.com/apps/social-blacklist](https://developers.reddit.com/apps/social-blacklist)
@@ -8,9 +13,11 @@ Moderators can install the app to their subreddit by going to [https://developer
 The settings page provides the following settings:
   1) Blacklisted-Domains: A list of domains to blacklist (For example: instagram.com, youtube.com)
   2) Removal message: Text that would be sent to the user on removal of the post.
+  4) Flag to enable/disable removing posts containing blacklisted domains found in `Social Links` section of user's profile.
+  4) Flag to enable/disable removing posts containing blacklisted domains found in post link.
+  4) Flag to enable/disable removing posts containing blacklisted domains found in post body/content.
   3) Flag to enable/disable mod-mail notification on removal.
-
-You can access these settings by going to https://developers.reddit.com/r/[subreddit-name]/apps/social-blacklist
+  8) Flag to enable/disable removing posts made by users having NSFW profiles
 
 ## Changelog
 * 0.0.2
@@ -20,3 +27,9 @@ You can access these settings by going to https://developers.reddit.com/r/[subre
 * 0.0.6
     1. Distinguish comment as 'MOD' and sticky.
     2. Add settings input for for enabling/disabling mod-mail notification on post removal.
+* 0.0.7
+    1. Add option to remove all posts by NSFW profiles.
+    2. Add option to remove posts when blacklisted domain is found in post link.
+    3. Add option to remove posts when blacklisted domain is found in post body.
+    4. Add option to igore posts made by moderators of the sub.
+    5. Update contents of mod-mail notification to contain author link
--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,59 @@
 import { Devvit, SettingScope} from '@devvit/public-api';
-
-enum removalReasons {
-  blacklistedDomainInSocialLinks = "Blacklisted domain found in user's Social Links",
-  blacklistedDomainInStickyPost = "BLacklisted domain found in user's Sticky post",
-  blacklistedDomainInRecentComments= "Blacklisted domain found in user's Recent comments",
-  NSFWProfile= "NSFW Profile",
-}
+import * as protos from "@devvit/protos";
+import { UserAboutResponse } from "@devvit/protos/types/devvit/plugin/redditapi/users/users_msg.js";
 
 Devvit.configure({redditAPI: true, 
                   redis: true,
                   userActions: false });
 
+enum removalReasons {
+  none = "None",
+  blacklistedDomainInSocialLinks = "Blacklisted domain found in user's Social Links",
+  blacklistedDomainInStickyPost = "BLacklisted domain found in user's Sticky post",
+  blacklistedDomainInPostLink = "BLacklisted domain found in post link",
+  blacklistedDomainInPostBody = "BLacklisted domain found in post body/content",
+  blacklistedDomainInRecentComments= "Blacklisted domain found in user's Recent comments",
+  NSFWProfile= "NSFW Profile",
+}
+
+export interface RedditAPIPlugins {
+    NewModmail: protos.NewModmail;
+    Widgets: protos.Widgets;
+    ModNote: protos.ModNote;
+    LinksAndComments: protos.LinksAndComments;
+    Moderation: protos.Moderation;
+    GraphQL: protos.GraphQL;
+    Listings: protos.Listings;
+    Flair: protos.Flair;
+    Wiki: protos.Wiki;
+    Users: protos.Users;
+    PrivateMessages: protos.PrivateMessages;
+    Subreddits: protos.Subreddits;
+}
+
+export type ExtendedDevvit = typeof Devvit & {
+    redditAPIPlugins: RedditAPIPlugins;
+};
+
+export function getExtendedDevvit (): ExtendedDevvit {
+    return Devvit as ExtendedDevvit; // The Devvit object already has the extended properties, they are simply not reflected in the public type definition.
+}
+
+async function getRawUserData (username: string, metadata: protos.Metadata): Promise<UserAboutResponse | undefined> {
+    try {
+        return await getExtendedDevvit().redditAPIPlugins.Users.UserAbout({ username }, metadata);
+    } catch {
+        return undefined;
+    }
+}
+
 Devvit.addSettings([
   {
     type: 'string',
     name: 'blacklisted-domains',
-    label: 'Blacklisted Social Domains (comma-separated):',
+    label: 'Blacklisted Domains (comma-separated):',
     scope: SettingScope.Installation,
-    helpText: "Provide a list of black-listed domains -  For example: instagram.com, youtube.com",
+    helpText: "Provide a list of black-listed domains - For example: instagram.com, youtube.com",
     onValidate: ({ value }) => {
       // Split the input and trim whitespace
       if (typeof value == "string") {
@@ -39,26 +75,52 @@ Devvit.addSettings([
     type: 'string',
     name: 'removal-message',
     label: 'Message to user on removal of the post:',
-    defaultValue: "Your post has been removed from this sub based on your profile information. Please review the rules of the sub for further information on what is allowed/disallowed on this subreddit.",
+    defaultValue: "Your post has been removed from this sub based on your profile information or post content. Please review rules of the sub for further information on what is allowed/disallowed on this subreddit.",
     scope: SettingScope.Installation,
   },
   {
     type: 'boolean',
+    name: 'removeDomainInSocialLinks',
+    label: 'Remove posts from users who have blacklisted domains in `Social Links` section of their profile.',
+    scope: SettingScope.Installation, 
+    defaultValue: true,
+  }, 
+  {
+    type: 'boolean',
+    name: 'removeDomainInPostLink',
+    label: 'Remove posts containing blacklisted domains in post link',
+    scope: SettingScope.Installation, 
+    defaultValue: false,
+  },
+  {
+    type: 'boolean',
+    name: 'removeDomainInPostBody',
+    label: 'Remove posts containing blacklisted domains in post body/content.',
+    scope: SettingScope.Installation, 
+    defaultValue: false,
+  },
+  {
+    type: 'boolean',
     name: 'notifyModeratorsOnPostRemoval',
-    label: 'Notify post removal through mod-mail:',
+    label: 'Notify moderators on post removal through mod-mail.',
     scope: SettingScope.Installation, 
     defaultValue: false
   },
-   /*
   {
     type: 'boolean',
     name: 'removeNSFWProfilePosts',
-    label: 'Remove posts made by NSFW profiles',
+    label: 'Remove all posts made by users having NSFW profiles(irrespective of content or Social Links).',
     scope: SettingScope.Installation, 
     defaultValue: false,
-    
   },
   {
+    type: 'boolean',
+    name: 'ignorePostsByModerators',
+    label: 'Ignore posts by moderators of this subreddit.',
+    scope: SettingScope.Installation, 
+    defaultValue: true,
+  },
+/*  {
     type: 'number',
     name: 'max-removals',
     label: 'Number of removals after which the user would be banned',
@@ -71,11 +133,18 @@ Devvit.addSettings([
 Devvit.addTrigger({
   event: 'PostSubmit',
   onEvent: async (event, context) => {
-    const blacklisted_list = await context.settings.get('blacklisted-domains');
-    const removal_message = await context.settings.get('removal-message')??'';
-    const subredditName = context.subredditName??'';
 
-    var removalReason:removalReasons = removalReasons.blacklistedDomainInSocialLinks ;
+    const settings = await context.settings.getAll();
+    const blacklisted_list = settings['blacklisted-domains'];
+    const removal_message = settings['removal-message'];
+    const removeDomainInSocialLinks = settings['removeDomainInSocialLinks'];
+    const removeNSFWProfilePosts = settings['removeNSFWProfilePosts'];
+    const notifyModeratorsOnPostRemoval = settings['notifyModeratorsOnPostRemoval'];
+    const removeDomainInPostLink = settings['removeDomainInPostLink'];
+    const removeDomainInPostBody = settings['removeDomainInPostBody'];
+    const ignorePostsByModerators = settings['ignorePostsByModerators'];
+    const subredditName = context.subredditName??'';
+    var removalReason:removalReasons = removalReasons.none;
 
     if( blacklisted_list && typeof blacklisted_list== "string") {
 
@@ -88,29 +157,45 @@ Devvit.addTrigger({
       var authorId = event.post?.authorId;
       if( typeof authorId == "string" && event.post!= undefined ) {
         const author = await context.reddit.getUserById(authorId);
-        var authorName = author?.username??"nobody";
-        var authorProfileIsNSFW = false;
+        var authorUsername = author?.username??"nobody";
 
-        // Fetch social links
-        const socialLinks = await author?.getSocialLinks(); // Returns array of UserSocialLink objects
+        const subreddit = await context.reddit.getCurrentSubreddit();
+        const moderators = await subreddit.getModerators().all();
+
+        if( ignorePostsByModerators ) {
+          for (const mod of  moderators) {
+            if (mod.username === authorUsername) {
+              return;//Do not action anything if the user is a moderator.
+            }
+          }
+        }
+
+        const socialLinks = await author?.getSocialLinks();
 
         // Check if any social link matches a blacklisted domain
         const blacklistedDomainFoundInSocialLinks = socialLinks?.some(link =>
           blacklisted_domains.some(domain => link.outboundUrl.toLowerCase().includes(domain))
         );
 
-        if (blacklistedDomainFoundInSocialLinks) {
+        const blacklistedDomainFoundInPostLink = blacklisted_domains.some(domain => event.post?.url.toLowerCase().includes(domain))
+        const blacklistedDomainFoundInPostBody = blacklisted_domains.some(domain => event.post?.selftext.toLowerCase().includes(domain))
+
+        const rawUserData = await getRawUserData(authorUsername, context.debug.metadata);
+
+        if (removeDomainInSocialLinks && blacklistedDomainFoundInSocialLinks) {
           removalReason = removalReasons.blacklistedDomainInSocialLinks;
         }
-        /*
-        else if (author?.nsfw) {
-          authorProfileIsNSFW = true;
-          removalReason = removalReasons.NSFWProfile;
-          console.log("Removing post as the user's profile is NSFW'");
+        else if( removeDomainInPostLink && blacklistedDomainFoundInPostLink ){
+          removalReason = removalReasons.blacklistedDomainInPostLink;
         }
-        */
+        else if( removeDomainInPostBody && blacklistedDomainFoundInPostBody ){
+          removalReason = removalReasons.blacklistedDomainInPostBody;
+        }
+        else if ( removeNSFWProfilePosts && rawUserData?.data && rawUserData.data.subreddit?.over18) {
+          removalReason = removalReasons.NSFWProfile;
+        }
 
-        if( authorProfileIsNSFW || blacklistedDomainFoundInSocialLinks )
+        if( removalReason != removalReasons.none )
         {
           const redditComment = await context.reddit.submitComment({
             id: event.post.id,
@@ -120,31 +205,25 @@ Devvit.addTrigger({
           await redditComment.distinguish(true);
 
           await context.reddit.sendPrivateMessage({
-            to: authorName,
+            to: authorUsername,
             subject: `Your post '${event.post.title}' has been removed from ${subredditName}`,
             text: `${removal_message} - post link: ${event.post.permalink}`,
           });
 
           const post =await context.reddit.getPostById(event.post.id);
-          const postRemoved = await post.remove();
-
-          const notifyModeratorsOnPostRemoval = await context.settings.get('notifyModeratorsOnPostRemoval');
+          await post.remove();
 
           if( notifyModeratorsOnPostRemoval ) {
             const conversationId = await context.reddit.modMail.createModNotification({  
               subject: 'post removal from Social-Blacklist',
-              bodyMarkdown: 'A post has been removed by Social-Blacklist.\n\n Post title: '+post.title+'\n\n Post link: '+post.permalink+'\n\n Removal reason: '+removalReason,
+              bodyMarkdown: 'A post has been removed by Social-Blacklist. \n\n Author: https://www.reddit.com'+author?.permalink+'  \n\n Post title: '+post.title+' \n\n Post link: '+post.permalink+'  \n\n Removal reason: '+removalReason,
               subredditId: context.subredditId,
             });
           }
-
         }
-        //TODO: Keep count of removals in redis, then ban user after X number of removals.   
-  
+        //TODO: Keep count of removals in redis, then ban user after X number of removals.
       }
-
     }
-
   },
 });
 
