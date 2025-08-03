ffe0011168b00b61dfc3265328e9ee60a263b290
Sharath <sharath@MacBook-Air.local>
2025-08-03 10:05:52 +0530

    Add banAfterRemovals setting for banning user after given number of removals

--- src/main.tsx
+++ src/main.tsx
@@ -212,14 +212,27 @@ Devvit.addSettings([
     scope: SettingScope.Installation, 
     defaultValue: true,
   },
-/*  {
-    type: 'number',
-    name: 'max-removals',
-    label: 'Number of removals after which the user would be banned',
-    scope: SettingScope.Installation,
-    defaultValue: 3
+  {
+    type: 'select',
+    name: 'banAfterRemovals',
+    label: 'Ban user after this many removals:',
+    options: [
+              {'label': 'Disabled', value: '0'},
+              {'label': '1', value: '1'},
+              {'label': '2', value: '2'},
+              {'label': '3', value: '3'},
+              {'label': '4', value: '4'},
+              {'label': '5', value: '5'},
+              {'label': '6', value: '6'},
+              {'label': '7', value: '7'},
+              {'label': '8', value: '8'},
+              {'label': '9', value: '9'},
+              {'label': '10', value: '10'},
+            ],
+    helpText: "Ban user after this many removals by Social-Blacklist app",
+    defaultValue: ['0'],
   }
-  */
+
 ]);
 
 Devvit.addTrigger({
